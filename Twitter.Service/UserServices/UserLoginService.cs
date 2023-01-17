097658dd3e804de7170ff172261215767daa240b
qod1rovou7 <shaxbozbekqodirov2006@gmail.com>
2023-01-18 03:02:57 +0500

    AsA

---
+++ Twitter.Service/UserServices/UserLoginService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twitter.Data.ProgramDbContext;
+
+namespace Twitter.Service.UserServices
+{
+    public class UserLoginService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public UserLoginService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public bool Login(string email, string password)
+        {
+            return applicationDbContext.Users.Any(x=>x.Email == email && x.Password == password);
+        }
+    }
+
+}
